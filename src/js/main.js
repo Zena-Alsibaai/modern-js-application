4758f78b5d6364b5f011a2bf639b4d0d5d65bd35
Zena-Alsibaai <zena.alsibaai@gmail.com>
2021-03-09 12:00:51 +0100

    revenir en arrière

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,13 @@
-// import {
-//   resetForm,
-//   affichageWindow,
-//   hiddenWindow,
-// } from "./modules/functions.js";
-
 (() => {
-  // ==============================La déclaration des variables=====================
   const createWindow = document.getElementById("id01"); // on passe le modal en variable
   const heroInfo = document.getElementById("info-hero");
+  console.log(heroInfo);
+
   const template = document.querySelector("#template");
   const target = document.querySelector("#target");
-  const url = "https://character-database.becode.xyz/characters";
 
-  // ========================Button foctions=================================
+  const url = ("https://character-database.becode.xyz/characters")
 
-  // button envoyer le new super hero à l'API
   // form vide
   function resetForm() {
     document.getElementById("newName").value = "";
@@ -23,27 +16,26 @@
     // document.getElementById("imgPreview").src = "";
     document.getElementById("newImg").value = "";
   }
-  //modal s affiche
+
   function affichageWindow(window) {
-    window.style.display = "block";
+    window.style.display = "block"; //modal s affiche
   }
-  //modal s enleve
+
   function hiddenWindow(window) {
-    window.style.display = "none";
+    window.style.display = "none"; //modal s enleve
   }
 
   function getValue() {
     const name = document.querySelector("#newName").value;
-    const shortDescription = document.querySelector("#shortDescription").value;
+    const shortDescription = document.querySelector("#shortDescription")
+      .value;
     const description = document.querySelector("#newDescription").value;
-    const image = document.querySelector("#imgPreview").src;
-    // console.log(image);
 
     let entrees = [];
-    let cles = ["name", "shortDescription", "description", "image"];
+    let cles = ["name", "shortDescription", "description"];
     let values = [];
 
-    values.push(name, shortDescription, description, image);
+    values.push(name, shortDescription, description);
 
     for (let i = 0; i < cles.length; i++) {
       entrees.push([cles[i], values[i]]);
@@ -51,45 +43,50 @@
 
     const newCard = Object.fromEntries(entrees);
 
-    return newCard;
+    return newCard
   }
+
   document
     .getElementById("createSubmit")
     .addEventListener("click", async (event) => {
       event.preventDefault();
-      getValue();
-      console.log(getValue());
 
+      getValue()
       try {
-        const response = await fetch(url, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(getValue()),
-        });
+        const response = await fetch(url,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(getValue()),
+          }
+        );
         //const freshHero = await response.json();
       } catch (error) {
         console.log(error);
       }
     });
-  // button créer super hero
+
   document
     .getElementById("createNewPerso")
     .addEventListener("click", function () {
       resetForm(); //affiche un formulaire vide
       affichageWindow(createWindow); //ouvre le modal sur la page
     });
-  // button fermer le modal
-  document.getElementById("close-btn").addEventListener("click", () => {
-    hiddenWindow(createWindow);
-    document.location.reload();
-  });
-  // button annuler
-  document.getElementById("cancel").addEventListener("click", function () {
-    hiddenWindow(createWindow);
-  });
-  // ========================La fonction principale=================================
+
+  document
+    .getElementById("close-btn")
+    .addEventListener("click", () => {
+      hiddenWindow(createWindow);
+      document.location.reload()
+    });
+
+  document
+    .getElementById("cancel")
+    .addEventListener("click", function () {
+      hiddenWindow(createWindow);
+    });
 
   //aller recuperer la liste de heros dans l api et l afficher, la supprimer ou la modifier
   async function getData() {
@@ -109,12 +106,15 @@
 
       clone.querySelector("#delete").addEventListener("click", async () => {
         console.log(id);
-        const response = await fetch(`${url}/${id}`, {
-          method: "DELETE",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
+        const response = await fetch(
+          `${url}/${id}`,
+          {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
         document.location.reload();
       });
 
@@ -122,44 +122,63 @@
         //console.log(id)
         affichageWindow(createWindow); //ouvre le modal sur la page
 
-        let response = await fetch(`${url}/${id}`);
+        let response = await fetch(
+          `${url}/${id}`
+        );
         const hero = await response.json();
 
         //affichage
         document.getElementById("newName").value = name;
         document.getElementById("shortDescription").value = shortDescription;
         document.getElementById("newDescription").value = description;
-        response = fetch(`${url}/${id}`, {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
+        console.log(hero);
+
+        response = fetch(
+          `${url}/${id}`,
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        //document.location.reload();
       });
 
-      clone.querySelector("#info-btn").addEventListener("click", async () => {
+      clone
+      .querySelector("#info-btn").
+      addEventListener("click", async () => {
         affichageWindow(heroInfo); //ouvre le modal sur la page
         console.log(name);
-        let response = await fetch(`${url}/${id}`);
+
+        let response = await fetch(
+          `${url}/${id}`
+        );
         const hero = await response.json();
 
         //affichage
-        document.getElementById("name-hero").value = name;
-        document.getElementById(
+        document
+        .getElementById("name-hero").value = name;
+
+        document
+        .getElementById(
           "shortDescription-hero"
         ).value = shortDescription;
 
         document.getElementById("description-hero").value = description;
 
-        response = fetch(`${url}/${id}`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
+        response = fetch(
+          `${url}/${id}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
       });
       target.appendChild(clone);
-    });
+    })
   }
   getData();
 })();
